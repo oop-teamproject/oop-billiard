843639874e8a911cee415a6d1c363db08c112a59
whitw <shkim7876@naver.com>
2019-11-03 09:34:05 +0900

    코드정리: CSphere 분리

--- virtualLego.cpp
+++ virtualLego.cpp
@@ -11,13 +11,13 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include "d3dUtility.h"
+#include "CSphere.h"
 #include <d3dx9.h>
 #include <vector>
 #include <ctime>
 #include <cstdlib>
 #include <cstdio>
 #include <cassert>
-
 IDirect3DDevice9* Device = NULL;
 
 // window size
@@ -37,196 +37,14 @@ D3DXMATRIX g_mWorld;
 D3DXMATRIX g_mView;
 D3DXMATRIX g_mProj;
 
-#define M_RADIUS 0.21   // ball radius (= 2.85cm, 1cm = 0.073684)
 #define GRAVITY_CONST 72.128 //gravity constant(=980cm/s = 72.128/s)
 // 10.097,  20.189
 #define PI 3.14159265
 #define M_HEIGHT 0.01 //height of wall
-#define DECREASE_RATE 0.9982 //rate of speed decrease
 
 // -----------------------------------------------------------------------------
 // CSphere class definition
 // -----------------------------------------------------------------------------
-
-class CSphere {
-private :
-	float					center_x, center_y, center_z;
-    float                   m_radius;
-	float					m_velocity_x;
-	float					m_velocity_y;
-	float					m_velocity_z;
-
-public:
-    CSphere(void)
-    {
-        D3DXMatrixIdentity(&m_mLocal); //m_mLocal은 클래스 맨 밑바닥에 있다. 로컬 좌표 변환(?)
-        ZeroMemory(&m_mtrl, sizeof(m_mtrl)); //m_mtrl은 클래스 맨 밑바닥에 있다. 구체의 표면?
-        m_radius = M_RADIUS;
-		m_velocity_x = 0;
-		m_velocity_y = 0;
-		m_velocity_z = 0;
-        m_pSphereMesh = NULL;
-    }
-    ~CSphere(void) {}
-
-public:
-    bool create(IDirect3DDevice9* pDevice, D3DXCOLOR color = d3d::WHITE)
-    {
-        if (NULL == pDevice)
-            return false;
-		
-        m_mtrl.Ambient  = color;
-        m_mtrl.Diffuse  = color;
-        m_mtrl.Specular = color;
-        m_mtrl.Emissive = d3d::BLACK;
-        m_mtrl.Power    = 5.0f;
-		
-        if (FAILED(D3DXCreateSphere(pDevice, getRadius(), 20, 20, &m_pSphereMesh, NULL)))
-            return false;
-        return true;
-    }
-	
-    void destroy(void)
-    {
-        if (m_pSphereMesh != NULL) {
-            m_pSphereMesh->Release();
-            m_pSphereMesh = NULL;
-        }
-    }
-
-    void draw(IDirect3DDevice9* pDevice, const D3DXMATRIX& mWorld)
-    {
-        if (NULL == pDevice)
-            return;
-        pDevice->SetTransform(D3DTS_WORLD, &mWorld);
-        pDevice->MultiplyTransform(D3DTS_WORLD, &m_mLocal);
-        pDevice->SetMaterial(&m_mtrl);
-		m_pSphereMesh->DrawSubset(0);
-    }
-	
-    bool hasIntersected(CSphere& ball) 
-	{
-		D3DXVECTOR3 other = ball.getCenter();
-		float dx = center_x - other.x;
-		float dy = center_y - other.y;
-		float dz = center_z - other.z;
-		float sumRad = getRadius() + ball.getRadius();
-		return (sumRad * sumRad) >= (dx * dx) + (dy * dy) + (dz * dz);
-	}
-	
-	void hitBy(CSphere& ball) 
-	{ 
-		if (!hasIntersected(ball))
-			return;
-		else
-		{
-			D3DXVECTOR3 other = ball.getCenter();
-			D3DXVECTOR3 vNorm = getCenter() - ball.getCenter();
-			D3DXVec3Normalize(&vNorm, &vNorm);
-			float dx = center_x - other.x;
-			float dy = center_y - other.y;
-			float dz = center_z - other.z;
-			float dsum = sqrt((dx * dx) + (dy * dy) + (dz * dz));
-			float diffx, diffy, diffz;
-			D3DXVECTOR3 diff;
-			if (dsum != 0)
-			{
-				diff = getRadius() * vNorm;
-				diffx = getRadius() * dx / dsum - dx / 2;
-				diffy = getRadius() * dy / dsum - dy / 2;
-				diffz = getRadius() * dz / dsum - dz / 2;
-			};
-			if (dsum == 0)
-			{
-				diffx = getRadius() / 2;//// 원래의 xyz 좌표의 차이
-				diffy = getRadius() / 2;
-				diffz = getRadius() / 2;
-
-			};
-			center_x = center_x + diffx;
-			center_y = center_y + diffy;/// 원래의 좌표만큼 서로밀어낸다.
-			center_z = center_z + diffz;
-			other.x = other.x - diffx;
-			other.y = other.y - diffy;
-			other.z = other.z - diffz;
-
-			 setCenter(center_x, center_y, center_z);
-			 ball.setCenter(other.x, other.y, other.z);
-			 //center 값 재조정 완료
-			 D3DXVECTOR3 vThisNorm = D3DXVec3Dot(&getVelocity(), &vNorm) * vNorm;
-			 D3DXVECTOR3 vThisOrtho = getVelocity() - vThisNorm;
-			 D3DXVECTOR3 vBallNorm = D3DXVec3Dot(&ball.getVelocity(), &vNorm) * vNorm;
-			 D3DXVECTOR3 vBallOrtho = ball.getVelocity() - vBallNorm;
-			 D3DXVECTOR3 vThisNewVec = vBallNorm + vThisOrtho;
-			 D3DXVECTOR3 vBallNewVec = vThisNorm + vBallOrtho;
-			 setPower(vThisNewVec.x, vThisNewVec.y, vThisNewVec.z);
-			 ball.setPower(vBallNewVec.x, vBallNewVec.y, vBallNewVec.z);
-		}
-	}
-
-	void ballUpdate(float timeDiff) /*timeDiff-- 초 단위*/
-	{
-		const float TIME_SCALE = 3.3;
-		D3DXVECTOR3 cord = this->getCenter();
-		double vx = abs(this->getVelocity_X());
-		double vy = abs(this->getVelocity_Y());
-		double vz = abs(this->getVelocity_Z());
-
-		if(vx > 0.01 || vy > 0.01 || vz > 0.01)
-		{
-			float tX = cord.x + TIME_SCALE*timeDiff*m_velocity_x;
-			float tY = cord.y + TIME_SCALE*timeDiff*m_velocity_y;
-			float tZ = cord.z + TIME_SCALE*timeDiff*m_velocity_z;
-			this->setCenter(tX, tY, tZ);
-		}
-		else { this->setPower(0,0);}
-		//this->setPower(this->getVelocity_X() * DECREASE_RATE, this->getVelocity_Z() * DECREASE_RATE);
-		double rate = 1 -  (1 - DECREASE_RATE)*timeDiff * 400;
-		if(rate < 0 )
-			rate = 0;
-		this->setPower(getVelocity_X() * rate/*, getVelocity_Y() - 0.3 * GRAVITY_CONST * timeDiff*/, getVelocity_Z() * rate);
-	}
-
-	double getVelocity_X() const { return this->m_velocity_x; }
-	double getVelocity_Y() const { return this->m_velocity_y; }
-	double getVelocity_Z() const { return this->m_velocity_z; }
-	D3DXVECTOR3 getVelocity() const { return D3DXVECTOR3(m_velocity_x, m_velocity_y, m_velocity_z); }
-
-	void setPower(double vx, double vz)
-	{
-		setPower(vx, getVelocity_Y(), vz);
-	}
-	void setPower(double vx, double vy, double vz)
-	{
-		this->m_velocity_x = vx;
-		this->m_velocity_y = vy;
-		this->m_velocity_z = vz;
-	}
-	void setCenter(float x, float y, float z)
-	{
-		D3DXMATRIX m;
-		center_x=x;	center_y=y;	center_z=z;
-		D3DXMatrixTranslation(&m, x, y, z);
-		setLocalTransform(m);
-	}
-	
-	float getRadius(void)  const { return this->m_radius;  }
-    const D3DXMATRIX& getLocalTransform(void) const { return m_mLocal; }
-    void setLocalTransform(const D3DXMATRIX& mLocal) { m_mLocal = mLocal; }
-    D3DXVECTOR3 getCenter(void) const
-    {
-        D3DXVECTOR3 org(center_x, center_y, center_z);
-        return org;
-    }
-	
-private:
-    D3DXMATRIX              m_mLocal;
-    D3DMATERIAL9            m_mtrl;
-    ID3DXMesh*              m_pSphereMesh;
-	
-};
-
-
 
 // -----------------------------------------------------------------------------
 // CWall class definition
